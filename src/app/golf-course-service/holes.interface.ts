24224d303bcad0a428b4dbe07d97a4833b3970e2
jared <jared.jacobson.2000@gmail.com>
2018-01-18 16:50:12 -0700

    created the Holes interface

---
+++ src/app/golf-course-service/holes.interface.ts
@@ -0,0 +1,28 @@
+export interface Holes {
+    course_id: number;
+    green_location: {
+        lat: number;
+        long: number;
+    };
+    hole_num: number;
+    id: number;
+    links: Object;
+    pin_location: Object;
+    tee_boxes: TeeBox[];
+}
+
+interface TeeBox {
+    course_hole_id: number;
+    hcp: number;
+    id: number;
+    location: {
+        lat: number;
+        lng: number;
+    };
+    meters: number;
+    par: number;
+    tee_color_type: string;
+    tee_hex_color: string;
+    tee_type: string;
+    yards: number;
+}
