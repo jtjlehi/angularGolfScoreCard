9be82d325c276c74333192c62f80ebfd45662b72
jared <jared.jacobson.2000@gmail.com>
2018-01-19 09:00:52 -0700

    made the base of the header template for the left part of the navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -7,17 +8,22 @@ import { AppComponent } from './app-component/app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GolfCourseService } from './golf-course-service/golf-course.service';
 import { HeaderComponent } from './header/header.component';
+import { MaterialModule } from './material/material.module';
+import { RightNavComponent } from './right-nav/right-nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RightNavComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [
     GolfCourseService
