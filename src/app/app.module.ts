d24a0b8ab965ff1ef49f14fd8e3c8dab24fa56c7
jared <jared.jacobson.2000@gmail.com>
2018-01-18 18:54:39 -0700

    created and tested header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app-component/app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GolfCourseService } from './golf-course-service/golf-course.service';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
