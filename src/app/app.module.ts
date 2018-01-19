a8482f5249da97274681742abd3114025db6736f
jared <jared.jacobson.2000@gmail.com>
2018-01-19 09:45:15 -0700

    created right and left nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { GolfCourseService } from './golf-course-service/golf-course.service';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './material/material.module';
+import { RightNavComponent } from './right-nav/right-nav.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RightNavComponent,
+    LeftNavComponent,
   ],
   imports: [
     BrowserModule,
