fa99b50c627c1559452fa83ad2a2c8fa3db1d251
jared <jared.jacobson.2000@gmail.com>
2018-01-29 11:43:38 -0700

    created a course-loader component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './material/material.module';
 import { RightNavComponent } from './right-nav/right-nav.component';
 import { LeftNavComponent } from './left-nav/left-nav.component';
+import { CourseLoaderComponent } from './course-loader/course-loader.component';
 
 
 @NgModule({
@@ -19,6 +20,7 @@ import { LeftNavComponent } from './left-nav/left-nav.component';
     HeaderComponent,
     RightNavComponent,
     LeftNavComponent,
+    CourseLoaderComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/course-loader/course-loader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'golf-course-loader',
+  templateUrl: './course-loader.component.html',
+  styleUrls: ['./course-loader.component.scss']
+})
+export class CourseLoaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
