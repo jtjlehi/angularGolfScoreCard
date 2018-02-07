1fb20ad88e435d18841b638969130ca464ec0d4b
jared <jared.jacobson.2000@gmail.com>
2018-02-07 16:43:58 -0700

    added getGameInfo and sendGameInfo to the firebase service

--- src/app/services/firebase/firebase.service.ts
+++ src/app/services/firebase/firebase.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http/src/client';
+import { Game } from './game.interface';
 
 @Injectable()
 export class FirebaseService {
@@ -7,5 +8,13 @@ export class FirebaseService {
   url: string;
 
   constructor(private httpClient: HttpClient) { }
+
+  sendGameInfo(gameInfoObject: Game) {
+    throw new Error('sendGameInfo not implemented yet');
+  }
+
+  getGameInfo() {
+    throw new Error('getGameInfo not implemented yet');
+  }
 
 }
