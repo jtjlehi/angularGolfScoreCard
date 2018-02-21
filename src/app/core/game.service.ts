34da2adf8ce050f55a388aef1d602afbd1e2b6fc
jared <jared.jacobson.2000@gmail.com>
2018-02-20 21:19:15 -0700

    added and implemented the addPlayer method

--- src/app/core/game.service.ts
+++ src/app/core/game.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import { AngularFirestore, DocumentChangeAction, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Game } from '../services/firebase/game.interface';
 import { GolfCourse } from '../golf-course-service/golf-course.interface';
+import { Player } from '../score-card/player.interface';
 
 @Injectable()
 export class GameService {
@@ -28,7 +29,6 @@ export class GameService {
     const game = {
       gameId: gameId,
       numOfHoles: course.hole_count,
-      players: [],
       holes: course.holes
     };
     this.afs.doc<Game>(`games/${gameId}`).set(game);
@@ -37,6 +37,10 @@ export class GameService {
 
   getGameData(gameId): Observable<Game> {
     return this.afs.doc<Game>(`games/${gameId}`).valueChanges();
+  }
+
+  addPlayer(gameId, player) {
+    this.afs.doc<Player>(`games/${gameId}`).collection(`player`).add(player);
   }
 
 }
