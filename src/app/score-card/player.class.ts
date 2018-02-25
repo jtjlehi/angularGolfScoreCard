4f390f22477509e80bd5698b0e50ebbccf51e6bf
jared <jared.jacobson.2000@gmail.com>
2018-02-24 20:59:19 -0700

    removed console.log() in the player.class

--- src/app/score-card/player.class.ts
+++ src/app/score-card/player.class.ts
@@ -13,11 +13,8 @@ export class Player implements PlayerI {
     public addHoles(holeCount: number): void {
         console.log('Player.holeCount: ', holeCount);
         for(let count = 0; count < holeCount; count ++) {
-            console.log(count);
-            console.log(this.hole_scores);
             this.hole_scores[count] = 0;
         }
-        console.log('this.hole_scores', this.hole_scores);
     }
 
 }
