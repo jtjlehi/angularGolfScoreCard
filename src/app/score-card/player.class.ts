963c0eca55cf805f9bd4eeaa8dd5383df4a6754f
jared <jared.jacobson.2000@gmail.com>
2018-02-24 20:51:26 -0700

    added an addHoles() method that initiates the holes in a player row.

--- src/app/score-card/player.class.ts
+++ src/app/score-card/player.class.ts
@@ -2,12 +2,22 @@ import { Player as PlayerI } from './player.interface';
 
 export class Player implements PlayerI {
 
-    public hole_scores: number[];
+    public hole_scores: number[] = [];
     public total: number = 0;
 
     constructor(
         public name: string,
         public handicap: number
     ) { }
+
+    public addHoles(holeCount: number): void {
+        console.log('Player.holeCount: ', holeCount);
+        for(let count = 0; count < holeCount; count ++) {
+            console.log(count);
+            console.log(this.hole_scores);
+            this.hole_scores[count] = 0;
+        }
+        console.log('this.hole_scores', this.hole_scores);
+    }
 
 }
