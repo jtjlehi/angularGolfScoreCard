845c268984e52301f1197c55409b6c99bab7619e
jared <jared.jacobson.2000@gmail.com>
2018-03-05 14:11:51 -0700

    added player as an @Input() for player-row.component

--- src/app/player-row/player-row.component.ts
+++ src/app/player-row/player-row.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Player } from '../score-card/player.interface';
 
 @Component({
   selector: 'golf-player-row',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerRowComponent implements OnInit {
 
+  @Input() player: Player;
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.player);
   }
 
 }
