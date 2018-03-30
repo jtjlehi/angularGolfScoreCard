154a41532867b3f74662297fd74e21a7836b080d
jared <jared.jacobson.2000@gmail.com>
2018-03-30 10:27:15 -0600

    updated environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCcGr9dOnBOdr69O78QBz2zM7NNCi_FFZ4",
+    authDomain: "angular-golf-scorecard.firebaseapp.com",
+    databaseURL: "https://angular-golf-scorecard.firebaseio.com",
+    projectId: "angular-golf-scorecard",
+    storageBucket: "angular-golf-scorecard.appspot.com",
+    messagingSenderId: "44692305268"
+  }
 };
