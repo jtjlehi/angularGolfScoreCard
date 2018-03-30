b408d603544c0a5209748c6a00296929885e247f
jared <jared.jacobson.2000@gmail.com>
2018-03-29 20:34:56 -0600

    added a randomly picked title to the end of game dialog.

--- src/app/end-game-dialog/end-game-dialog.component.ts
+++ src/app/end-game-dialog/end-game-dialog.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EndGameDialogComponent implements OnInit {
 
+  endPhrases: string[] = [
+    "It's over",
+    "Lets see how you did",
+    "The End",
+    "Wait it's over?",
+    "How'd you do?",
+    "Not to shabby"
+  ];
+  endPhrase: string;
+
   constructor() { }
 
   ngOnInit() {
+    const randIndex = Math.floor(Math.random() * this.endPhrases.length);
+    this.endPhrase = this.endPhrases[randIndex];
   }
 
 }
